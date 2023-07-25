2391a5d6ec2e732c7f084ab91ea3812474a05040
Nabil160 <nbakhunjee97@gmail.com>
2023-07-25 22:52:02 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# task_manager
+
+## Task Manager mobile app
+#### Flutter, Rest API
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
